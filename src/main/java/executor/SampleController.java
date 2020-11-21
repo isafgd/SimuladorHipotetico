c5110dce3865cc5ef6650e63efe4125e5f642e3e
Gianluccagit config --global user.name Gianluccagit config --global user.name Gianlucca <gdmbuzo@inf.ufpel.edu.br>
2020-11-21 19:16:04 -0300

    modificando a entrada do execution mode

--- src/main/java/executor/SampleController.java
+++ src/main/java/executor/SampleController.java
@@ -16,9 +16,9 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
 import lombok.Data;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
+
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -37,6 +37,9 @@ public class SampleController implements Initializable {
     private Memory memory = new Memory(list);
 
     private CPU cpu = new CPU();
+
+    @FXML
+    private TextArea console;
 
     @FXML
     private TextArea instructions;
@@ -72,65 +75,28 @@ public class SampleController implements Initializable {
     public void onInsertTextAction() throws IOException, InterruptedException {
         Read reader = new Read();
         instrucoesLeitura = instructions.getText().replaceAll("\n", System.getProperty("line.separator"));
-        String teste = executionMode.getValue();
+        String executionModeValue = executionMode.getValue();
         Writer.writeFile(instrucoesLeitura);
         //Covnverte bonito
         //imprimir na caixa de texto
         instructions.clear();
         System.out.println(instrucoesLeitura);
-        System.out.println(teste);
         cpu.initialMemory(memory,list);
-        cpu.executionMode(memory, reader, i, list);
+        cpu.executionMode(memory, reader, executionModeValue, list);
     }
-
-
-    /*@FXML
-    public void changeScreenButtonPushed (ActionEvent event) throws IOException {
-        Parent tableViewParent = FXMLLoader.load(getClass().getResource("/fxml/initializer.fxml"));
-        Scene tableViewScene = new Scene(tableViewParent);
-        Stage window = (Stage) ((javafx.scene.Node)event.getSource()).getScene().getWindow();
-        window.setScene(tableViewScene);
-        window.show();
-    }*/
 
     @FXML
     public void onBotaoStepAciotn(){
         i++;
         System.out.println(i);
+        console.setText("QlqrCoisa");
     }
 
     //Esse metodo tem que retornar como um ObservableList
 
     public ObservableList<Registradores> getRegistradores(){
-        /*StackPointer SP = (StackPointer) memory.get(13);
-        ProgramCounter PC = (ProgramCounter) memory.get(14);
-        Accumulator ACC = (Accumulator) memory.get(15);
-        OperationMode MOP = (OperationMode) memory.get(16);
-        AddressRecorder RE = (AddressRecorder) memory.get(18);
-        InstructionRecorder RI = (InstructionRecorder) memory.get(17);*/
-/*
-        list.add(new Registradores("0",0));
-        list.add(new Registradores("1",0));
-        list.add(new Registradores("2",10));
-        list.add(new Registradores("3",0));
-        list.add(new Registradores("4",0));
-        list.add(new Registradores("5",0));
-        list.add(new Registradores("6",0));
-        list.add(new Registradores("7",0));
-        list.add(new Registradores("8",0));
-        list.add(new Registradores("9",0));
-        list.add(new Registradores("10",0));
-        list.add(new Registradores("11",0));
-        list.add(new Registradores("12",0));
-        list.add(new Registradores("SP", SP.getPointer()));
-        list.add(new Registradores("PC", PC.getPc()));
-        list.add(new Registradores("ACC",ACC.getAcc()));
-        list.add(new Registradores("MOP",MOP.getMop()));
-        list.add(new Registradores("RI",RI.getRi()));
-        list.add(new Registradores("RE",RE.getRe()));
-        list.add(new Registradores("OPD1",(Integer) memory.get(19)));*/
-
         return list;
     }
+
 
 }
