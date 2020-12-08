6981006968d816b3a48f2246e13f3923d28022c2
Jonnhymoraes <moraesjonnhy@gmail.com>
2020-12-08 15:31:35 -0300

    adicionei carregador

---
+++ src/main/java/charger/charger.java
@@ -0,0 +1,19 @@
+package charger;
+
+import executor.Memory;
+import executor.MemoryList;
+import executor.Operations;
+
+import java.util.HashMap;
+
+public class Charger extends Operations {
+    private MemoryList;
+    private Memory memory;
+
+    public Charger(){
+        adress = new HashMap<>()
+    }//Carregador
+
+    public Charger carrega(String)
+
+}
