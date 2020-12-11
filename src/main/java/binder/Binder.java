5a68bcb5eecd576145b53f16b748e2a5ba8cfae1
Isabela Dias <dias.isabela@scalasystems.com.br>
2020-12-10 21:03:26 -0300

    iniciando o ligador

--- src/main/java/binder/Binder.java
+++ src/main/java/binder/Binder.java
@@ -1,37 +1,57 @@
-/*package binder;
+package binder;
 
-
-import config.GetProperties;
 import executor.Reader;
+import executor.Writer;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Binder {
-
-    private GetProperties properties;
 
     public void firstPassage () throws IOException {
         Reader reader = new Reader("OutPut1Montador.txt");
         Reader reader2 = new Reader("OutPut1Montador.txt");
 
-        FileWriter arq;
-        File output = new File("src/main/resources/txt/OutPutLigador.txt");
         StringBuilder instructions = new StringBuilder();
-
         String line = reader.readLine();
 
         while (line!=null){
-            try {
-                arq = new FileWriter(output);
-                arq.write(instructions);
-                arq.flush();
-                arq.close();
-            } catch (IOException ex) {
-                System.out.println("Arquivo nao encontrado");
+            String newLine = null;
+            //newLine = line.substring(0,16);
+            int addressMode = Integer.parseInt(line.substring(8, 11), 2);
+            if(line.length()==32){
+                if(addressMode==4){
+                    newLine = line;
+                }
+                else{
+                    int opd = Integer.parseInt(line.substring(16, 32),  2);
+                    int newOpd = opd + 240;
+                    String binOpd = toBinary(newOpd,16);
+                    newLine = line.substring(0,16) + binOpd;
+                }
+            }else if (line.length()==48){
+                
             }
+            instructions.append(newLine+"\n");
+            line = reader.readLine();
         }
+
+        line = reader2.readLine();
+
+        while (line!=null){
+            instructions.append(line+"\n");
+            line = reader.readLine();
+        }
+
+        Writer.writeFile(instructions.toString(), "OutPutLigador.txt");
     }
 
-}*/
+    public String toBinary(int x, int len) {
+
+        if (len > 0) {
+            return String.format("%" + len + "s",
+                    Integer.toBinaryString(x)).replaceAll(" ", "0");
+        }
+        return null;
+    }
+
+}
